745c8704df7db265ee774f15ba5ebe57bda1a3bc
jaehyo <chojh0621@gmail.com>
2023-01-01 02:16:26 +0000

    add ocrnet_hr18

---
+++ CV03/configs/ocrnet/ocrnet_hr18_512x512_20k_voc12aug.py
@@ -0,0 +1,65 @@
+import wandb
+
+_base_ = [
+    '/opt/ml/CV03/mmsegmentation/configs/_base_/models/ocrnet_hr18.py',
+    '/opt/ml/CV03/CV03/_base_/custom.py', 
+    '/opt/ml/CV03/mmsegmentation/configs/_base_/default_runtime.py',
+    '/opt/ml/CV03/CV03/_base_/scheduler_epochs_60.py'
+]
+norm_cfg = dict(type='SyncBN', requires_grad=True)
+model = dict(decode_head=[
+    dict(
+        type='FCNHead',
+        in_channels=[18, 36, 72, 144],
+        channels=sum([18, 36, 72, 144]),
+        in_index=(0, 1, 2, 3),
+        input_transform='resize_concat',
+        kernel_size=1,
+        num_convs=1,
+        concat_input=False,
+        dropout_ratio=-1,
+        num_classes=11,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=0.4)),
+    dict(
+        type='OCRHead',
+        in_channels=[18, 36, 72, 144],
+        in_index=(0, 1, 2, 3),
+        input_transform='resize_concat',
+        channels=512,
+        ocr_channels=256,
+        dropout_ratio=-1,
+        num_classes=11,
+        norm_cfg=norm_cfg,
+        align_corners=False,
+        loss_decode=dict(
+            type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0)),
+])
+
+wandb.login()
+# yapf:disable
+log_config = dict(
+    interval=100,
+    hooks=[
+        dict(type='TextLoggerHook', by_epoch=True),
+        dict(type='WandbLoggerHook',interval=100,
+            init_kwargs=dict(
+                project='Segmentation_project',
+                entity = 'aitech4_cv3',
+                name = "ocrnet_hr18"),)
+        # log_checkpoint=True,
+        # log_checkpoint_metadata=True,
+        # dict(type='TensorboardLoggerHook')
+        # dict(type='PaviLoggerHook') # for internal services
+    ])
+
+
+# yapf:enable
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
+cudnn_benchmark = True
